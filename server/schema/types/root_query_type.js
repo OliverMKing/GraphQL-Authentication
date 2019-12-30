274c71e1dc1a8768315ca4718f9fab9f6d7d7eac
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2019-12-30 16:32:32 -0500

    Added currentUser root query

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,10 +1,16 @@
 const graphql = require("graphql");
 const { GraphQLObjectType } = graphql;
+const UserType = require("./user_type");
 
 const RootQueryType = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
-    dummyField: { type: graphql.GraphQLID }
+    currentUser: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        return req.user;
+      }
+    }
   }
 });
 
