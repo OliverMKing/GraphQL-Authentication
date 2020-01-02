c45a0ee343055a1d6b1881fb2660208cabcb693d
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2020-01-01 22:58:22 -0500

    Added autoredirect on login (and signup)

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -1,17 +1,15 @@
 import React, { Component } from "react";
 import AuthForm from "./AuthForm";
 import { graphql } from "react-apollo";
-import mutation from "../mutations/Login";
+import mutation from "../mutations/Signup";
 import query from "../queries/CurrentUser";
 import { hashHistory } from "react-router";
 
-class LoginForm extends Component {
+class SignupForm extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      errors: []
-    };
+    this.state = { errors: [] };
   }
 
   componentWillUpdate(nextProps) {
@@ -36,7 +34,7 @@ class LoginForm extends Component {
   render() {
     return (
       <div>
-        <h3>Login</h3>
+        <h3>Sign Up</h3>
         <AuthForm
           errors={this.state.errors}
           onSubmit={this.onSubmit.bind(this)}
@@ -46,4 +44,4 @@ class LoginForm extends Component {
   }
 }
 
-export default graphql(query)(graphql(mutation)(LoginForm));
+export default graphql(query)(graphql(mutation)(SignupForm));
